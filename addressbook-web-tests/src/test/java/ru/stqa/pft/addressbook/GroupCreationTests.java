77e56b8b3654e51c53978f4e1b5e5c8f7721676e
pgujva <pgujva@usetech.ru>
2018-07-04 18:44:54 +0300

    GroupCreationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -0,0 +1,57 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class GroupCreationTests {
+    FirefoxDriver wd;
+
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void GroupCreationTests() throws Exception {
+        wd.get("http://localhost/addressbook/");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        wd.findElement(By.linkText("groups")).click();
+        wd.findElement(By.name("new")).click();
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys("тест1");
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys("тест2");
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys("тест3");
+        wd.findElement(By.name("submit")).click();
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        wd.quit();
+
+    }
+
+
+}
+
