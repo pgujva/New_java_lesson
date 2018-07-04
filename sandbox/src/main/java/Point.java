d615dbd0dfeb0095d1cbde991123e0b0ea5adc53
pgujva <pgujva@usetech.ru>
2018-07-04 15:49:11 +0300

    1

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,16 +1,18 @@
 
 public class Point {
-    double x;
-    double y;
+    public double x;
+    public double y;
+
+   public Point(double x,double y) {
+        this.x=x;
+       this.y=y;
+
+    }
 
     public static void main (String[] args) {
-        Point p1 = new Point();
-        p1.x=5;
-        p1.y=7;
+        Point p1 = new Point(5,7);
 
-        Point p2 = new Point();
-        p1.x=10;
-        p2.y=90;
+        Point p2 = new Point(4444,222);
 
         System.out.println(distance(p1,p2));
     }
