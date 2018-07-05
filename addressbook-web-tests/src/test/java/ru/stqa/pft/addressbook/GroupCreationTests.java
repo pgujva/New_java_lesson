761f9289695820062e77b3872bc78cd6dcec9f05
pgujva <pgujva@usetech.ru>
2018-07-05 11:56:40 +0300

    Перенос входа в beforemethod

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,35 +1,34 @@
 package ru.stqa.pft.addressbook;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
 public class GroupCreationTests {
-    FirefoxDriver wd;
+    ChromeDriver wd;
 
 
     @BeforeMethod
     public void setUp() throws Exception {
-        wd = new FirefoxDriver();
+        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void GroupCreationTests() throws Exception {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void testGroupCreation() throws Exception {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
@@ -46,12 +45,16 @@ public class GroupCreationTests {
     }
 
 
-    @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wd.quit();
+    @AfterMethod
+    public void tearDown() {wd.quit(); }
 
-    }
-
-
+   public static boolean isAlertPresent(ChromeDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        }catch (NoAlertPresentException e) {
+            return false;
+        }
+   }
 }
 
