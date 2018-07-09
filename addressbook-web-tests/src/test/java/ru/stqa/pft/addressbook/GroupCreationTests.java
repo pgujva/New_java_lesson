743ce523acce729b06f29ae24fd92a53cb9f5a42
pgujva <pgujva@usetech.ru>
2018-07-09 11:29:49 +0300

    создание класса Testbase, перенос методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -8,27 +8,8 @@ import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
-public class GroupCreationTests {
-  ChromeDriver wd;
+public class GroupCreationTests extends TestBase{
 
-
-  @BeforeMethod
-  public void setUp() throws Exception {
-    wd = new ChromeDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
 
   @Test
   public void testGroupCreation() throws Exception {
@@ -39,47 +20,6 @@ public class GroupCreationTests {
       returnToGroupPage();
   }
 
-    private void returnToGroupPage() {
-        wd.findElement(By.linkText("group page")).click();
-    }
 
-    private void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    private void initGroupCreation() {
-        wd.findElement(By.name("new")).click();
-    }
-
-    private void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
-    }
-
-
-    @AfterMethod
-  public void tearDown() {
-    wd.quit();
-  }
-
-  public static boolean isAlertPresent(ChromeDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
 
