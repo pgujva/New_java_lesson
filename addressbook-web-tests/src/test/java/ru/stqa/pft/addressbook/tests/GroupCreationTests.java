3f7a4a07c73ae3274ac09f1e4bac13f2170b4fd7
pgujva <pgujva@usetech.ru>
2018-07-10 22:47:27 +0300

    22

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -10,11 +10,11 @@ public class GroupCreationTests extends TestBase{
 
   @Test
   public void testGroupCreation() throws Exception {
-      app.gotoGroupPage();
-      app.initGroupCreation();
-      app.fillGroupForm(new GroupData("тест1", "тест2", "тест3"));
-      app.submitGroupCreation();
-      app.returnToGroupPage();
+      app.getNavigationHelper().gotoGroupPage();
+      app.getGroupHelper().initGroupCreation();
+      app.getGroupHelper().fillGroupForm(new GroupData("тест1", "тест2", "тест3"));
+      app.getGroupHelper().submitGroupCreation();
+      app.getGroupHelper().returnToGroupPage();
   }
 
 
