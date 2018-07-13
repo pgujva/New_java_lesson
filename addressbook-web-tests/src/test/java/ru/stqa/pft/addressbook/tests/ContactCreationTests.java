037b838fe7c7e57b2440d89dc8a4c584ded6ed6f
pgujva <pgujva@usetech.ru>
2018-07-13 12:30:21 +0300

    выделение вспомогательный методов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
+
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class ContactCreationTests extends TestBase {
   ChromeDriver wd;
@@ -18,48 +15,70 @@ public class ContactCreationTests extends TestBase {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testContactsCreation() throws Exception {
 
-    wd.findElement(By.linkText("add new")).click();
+    gotoContactCreationPage();
+    fillContactForm(new ContactData("testname", "testLastname", "testaddress",
+            "1111", "222", "333", "1testemail",
+            "2testemail", "3testemail"));
+    sumbitContactCreation();
+    goToHomePage();
+  }
+
+  private void goToHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  private void sumbitContactCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillContactForm(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("testname");
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("testLastname");
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("testaddress");
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("1111");
+    wd.findElement(By.name("home")).sendKeys(contactData.getHomephone());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("222");
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilephone());
     wd.findElement(By.name("work")).click();
     wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys("333");
+    wd.findElement(By.name("work")).sendKeys(contactData.getWorkphone());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("1testemail");
+    wd.findElement(By.name("email")).sendKeys(contactData.getFirstEmail());
     wd.findElement(By.name("email2")).click();
     wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys("2testemail");
+    wd.findElement(By.name("email2")).sendKeys(contactData.getSecondEmail());
     wd.findElement(By.name("email3")).click();
     wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys("3testemail");
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    //wd.findElement(By.linkText("home")).click();
+    wd.findElement(By.name("email3")).sendKeys(contactData.getThirdEmail());
+  }
+
+  private void gotoContactCreationPage() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactData.java
@@ -0,0 +1,61 @@
+package ru.stqa.pft.addressbook.tests;
+
+public class ContactData {
+  private final String name;
+  private final String lastname;
+  private final String address;
+  private final String homephone;
+  private final String mobilephone;
+  private final String workphone;
+  private final String firstEmail;
+  private final String secondEmail;
+  private final String thirdEmail;
+
+  public ContactData(String name, String lastname, String address, String homephone, String mobilephone, String workphone, String firstEmail, String secondEmail, String thirdEmail) {
+    this.name = name;
+    this.lastname = lastname;
+    this.address = address;
+    this.homephone = homephone;
+    this.mobilephone = mobilephone;
+    this.workphone = workphone;
+    this.firstEmail = firstEmail;
+    this.secondEmail = secondEmail;
+    this.thirdEmail = thirdEmail;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomephone() {
+    return homephone;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
+  public String getWorkphone() {
+    return workphone;
+  }
+
+  public String getFirstEmail() {
+    return firstEmail;
+  }
+
+  public String getSecondEmail() {
+    return secondEmail;
+  }
+
+  public String getThirdEmail() {
+    return thirdEmail;
+  }
+}
